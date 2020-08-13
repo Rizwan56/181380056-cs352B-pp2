6e2b4cc63d1eb51f87bb7617bdcced9eb6d644a8
Rizwan56 <67236613+Rizwan56@users.noreply.github.com>
2020-08-13 02:49:04 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee[] emp = new Employee[6];
+
+        SalariedEmployee s1 = new SalariedEmployee(1 , "Rizwan" , 40.0 , 10000.0);
+        SalariedEmployee s2 = new SalariedEmployee(2 , "Umer" , 50.0 , 15000.0);
+
+        HourlyEmployee h1 = new HourlyEmployee(3 , "Khadijah" , 60.0 , 10.5 , 100.0);
+        HourlyEmployee h2 = new HourlyEmployee(4 , "Amir" , 70.0 , 15.0 , 150.0);
+
+        CommissionedEmployee c1 = new CommissionedEmployee(5 , "Atif" , 80.0 , 10000.0 , 500.0);
+        CommissionedEmployee c2 = new CommissionedEmployee(6 , "Numan" , 90.0 , 15000.0 , 100.0);
+
+
+
+        emp[0] = s1;
+        emp[1] = s2;
+        emp[2] = h1;
+        emp[3] = h2;
+        emp[4] = c1;
+        emp[5] = c2;
+
+        for (int i = 0 ; i < emp.length ; i++)
+        {
+            System.out.println();
+            System.out.println(emp[i].toString());
+        }
+    }
+}
